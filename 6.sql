bb92e733cf223277b733d270d4e994242dc096f6
Jingwen <izumiyang@gmail.com>
2020-04-30 15:10:31 +0200

    commit

--- 6.sql
+++ 6.sql
@@ -1,127 +1,180 @@
--- MySQL Workbench Forward Engineering
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: optica
+-- ------------------------------------------------------
+-- Server version	8.0.19
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- -----------------------------------------------------
--- Schema Optica
--- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `Optica` ;
+--
+-- Table structure for table `cliente`
+--
 
--- -----------------------------------------------------
--- Schema Optica
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `Optica` DEFAULT CHARACTER SET utf8 ;
-USE `Optica` ;
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cliente` (
+  `idCliente` int NOT NULL AUTO_INCREMENT,
+  `direccion` varchar(45) NOT NULL,
+  `telefono` varchar(45) NOT NULL,
+  `correo_elec` varchar(45) NOT NULL,
+  `fecha_regi` datetime NOT NULL,
+  `cliente_recomenda` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idCliente`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- -----------------------------------------------------
--- Table `Optica`.`Direccion`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `Optica`.`Direccion` ;
+--
+-- Dumping data for table `cliente`
+--
 
-CREATE TABLE IF NOT EXISTS `Optica`.`Direccion` (
-  `idDireccion` INT NOT NULL AUTO_INCREMENT,
-  `calle` VARCHAR(45) NOT NULL,
-  `num_calle` INT NOT NULL,
-  `piso` INT NOT NULL,
-  `puerta` INT NULL,
-  `ciudad` VARCHAR(45) NOT NULL,
-  `codigo_p` INT NOT NULL,
-  `país` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`idDireccion`))
-ENGINE = InnoDB;
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES (1,'calle llorer,59,4,4','65892353','izumiyang@gmail.com','2019-02-03 00:00:00','Hoan sanchz');
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `direc_proveedor`
+--
 
--- -----------------------------------------------------
--- Table `Optica`.`Proveedor`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `Optica`.`Proveedor` ;
+DROP TABLE IF EXISTS `direc_proveedor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `direc_proveedor` (
+  `idDireccion` int NOT NULL AUTO_INCREMENT,
+  `calle` varchar(45) NOT NULL,
+  `num_calle` int NOT NULL,
+  `piso` int NOT NULL,
+  `puerta` int DEFAULT NULL,
+  `ciudad` varchar(45) NOT NULL,
+  `codigo_p` int NOT NULL,
+  `país` varchar(45) NOT NULL,
+  PRIMARY KEY (`idDireccion`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS `Optica`.`Proveedor` (
-  `idProveedor` INT NOT NULL AUTO_INCREMENT,
-  `nombre` VARCHAR(45) NOT NULL,
-  `telefono` VARCHAR(45) NOT NULL,
-  `fax` VARCHAR(45) NOT NULL,
-  `nif` VARCHAR(45) NOT NULL,
-  `Direccion_idDireccion` INT NOT NULL,
-  PRIMARY KEY (`idProveedor`),
-  INDEX `fk_Proveedor_Direccion_idx` (`Direccion_idDireccion` ASC) VISIBLE,
-  CONSTRAINT `fk_Proveedor_Direccion`
-    FOREIGN KEY (`Direccion_idDireccion`)
-    REFERENCES `Optica`.`Direccion` (`idDireccion`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+--
+-- Dumping data for table `direc_proveedor`
+--
 
+LOCK TABLES `direc_proveedor` WRITE;
+/*!40000 ALTER TABLE `direc_proveedor` DISABLE KEYS */;
+INSERT INTO `direc_proveedor` VALUES (1,'Mas',43,5,3,'ALBACETE',8934,'ESPAÑA');
+/*!40000 ALTER TABLE `direc_proveedor` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- -----------------------------------------------------
--- Table `Optica`.`Cliente`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `Optica`.`Cliente` ;
+--
+-- Table structure for table `empleado`
+--
 
-CREATE TABLE IF NOT EXISTS `Optica`.`Cliente` (
-  `idCliente` INT NOT NULL AUTO_INCREMENT,
-  `direccion` VARCHAR(45) NOT NULL,
-  `telefono` VARCHAR(45) NOT NULL,
-  `correo_elec` VARCHAR(45) NOT NULL,
-  `fecha_regi` DATETIME NOT NULL,
-  `cliente_recomenda` VARCHAR(45) NULL,
-  PRIMARY KEY (`idCliente`))
-ENGINE = InnoDB;
+DROP TABLE IF EXISTS `empleado`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `empleado` (
+  `idEmpleado` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  PRIMARY KEY (`idEmpleado`),
+  UNIQUE KEY `idEmpleado_UNIQUE` (`idEmpleado`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `empleado`
+--
 
--- -----------------------------------------------------
--- Table `Optica`.`Empleado`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `Optica`.`Empleado` ;
+LOCK TABLES `empleado` WRITE;
+/*!40000 ALTER TABLE `empleado` DISABLE KEYS */;
+INSERT INTO `empleado` VALUES (1,'Ana','Garcia');
+/*!40000 ALTER TABLE `empleado` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE IF NOT EXISTS `Optica`.`Empleado` (
-  `idEmpleado` INT NOT NULL,
-  PRIMARY KEY (`idEmpleado`))
-ENGINE = InnoDB;
+--
+-- Table structure for table `gafa`
+--
 
-
--- -----------------------------------------------------
--- Table `Optica`.`Gafa`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `Optica`.`Gafa` ;
-
-CREATE TABLE IF NOT EXISTS `Optica`.`Gafa` (
-  `idGafa` INT NOT NULL AUTO_INCREMENT,
-  `provedor` VARCHAR(45) NOT NULL,
-  `Proveedor_idProveedor` INT NOT NULL,
-  `marca` VARCHAR(45) NOT NULL,
-  `cristal_gradu_der` VARCHAR(45) NOT NULL,
-  `cristal_gradu_izq` VARCHAR(45) NOT NULL,
-  `marco` VARCHAR(45) NOT NULL,
-  `cristal_color_der` VARCHAR(45) NOT NULL,
-  `cristal_color_izq` VARCHAR(45) NOT NULL,
-  `precio` INT NOT NULL,
-  `Cliente_idCliente` INT NOT NULL,
-  `Empleado_idEmpleado` INT NOT NULL,
+DROP TABLE IF EXISTS `gafa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `gafa` (
+  `idGafa` int NOT NULL AUTO_INCREMENT,
+  `Proveedor_idProveedor` int NOT NULL,
+  `marca` varchar(45) NOT NULL,
+  `cristal_gradu_der` varchar(45) NOT NULL,
+  `cristal_gradu_izq` varchar(45) NOT NULL,
+  `marco` varchar(45) NOT NULL,
+  `cristal_color_der` varchar(45) NOT NULL,
+  `cristal_color_izq` varchar(45) NOT NULL,
+  `precio` int NOT NULL,
+  `Cliente_idCliente` int DEFAULT NULL,
+  `Empleado_idEmpleado` int DEFAULT NULL,
   PRIMARY KEY (`idGafa`),
-  INDEX `fk_Gafa_Proveedor1_idx` (`Proveedor_idProveedor` ASC) VISIBLE,
-  INDEX `fk_Gafa_Cliente1_idx` (`Cliente_idCliente` ASC) VISIBLE,
-  INDEX `fk_Gafa_Empleado1_idx` (`Empleado_idEmpleado` ASC) VISIBLE,
-  CONSTRAINT `fk_Gafa_Proveedor1`
-    FOREIGN KEY (`Proveedor_idProveedor`)
-    REFERENCES `Optica`.`Proveedor` (`idProveedor`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Gafa_Cliente1`
-    FOREIGN KEY (`Cliente_idCliente`)
-    REFERENCES `Optica`.`Cliente` (`idCliente`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Gafa_Empleado1`
-    FOREIGN KEY (`Empleado_idEmpleado`)
-    REFERENCES `Optica`.`Empleado` (`idEmpleado`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  KEY `fk_Gafa_Proveedor1_idx` (`Proveedor_idProveedor`),
+  KEY `fk_Gafa_Cliente1_idx` (`Cliente_idCliente`),
+  KEY `fk_Gafa_Empleado1_idx` (`Empleado_idEmpleado`),
+  CONSTRAINT `fk_Gafa_Cliente1` FOREIGN KEY (`Cliente_idCliente`) REFERENCES `cliente` (`idCliente`),
+  CONSTRAINT `fk_Gafa_Empleado1` FOREIGN KEY (`Empleado_idEmpleado`) REFERENCES `empleado` (`idEmpleado`),
+  CONSTRAINT `fk_Gafa_Proveedor1` FOREIGN KEY (`Proveedor_idProveedor`) REFERENCES `proveedor` (`idProveedor`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `gafa`
+--
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+LOCK TABLES `gafa` WRITE;
+/*!40000 ALTER TABLE `gafa` DISABLE KEYS */;
+INSERT INTO `gafa` VALUES (1,1,'Raybon','2','2','metal','Pink','Pink',40,1,NULL),(2,1,'Montblanc','0','0','metal','Black','Black',98,NULL,1);
+/*!40000 ALTER TABLE `gafa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `proveedor`
+--
+
+DROP TABLE IF EXISTS `proveedor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `proveedor` (
+  `idProveedor` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `telefono` varchar(45) NOT NULL,
+  `fax` varchar(45) NOT NULL,
+  `nif` varchar(45) NOT NULL,
+  `Direccion_idDireccion` int NOT NULL,
+  PRIMARY KEY (`idProveedor`),
+  KEY `fk_Proveedor_Direccion_idx` (`Direccion_idDireccion`),
+  CONSTRAINT `fk_Proveedor_Direccion` FOREIGN KEY (`Direccion_idDireccion`) REFERENCES `direc_proveedor` (`idDireccion`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `proveedor`
+--
+
+LOCK TABLES `proveedor` WRITE;
+/*!40000 ALTER TABLE `proveedor` DISABLE KEYS */;
+INSERT INTO `proveedor` VALUES (1,'OpticaGonzalez','987324098','987324099','A78430380G',1);
+/*!40000 ALTER TABLE `proveedor` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-04-30 14:54:34
